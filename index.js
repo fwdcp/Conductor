071e26aa08e6ccce0cedf354729d41cd3aa85d30
thesupremecommander <thesupremecmdr@gmail.com>
2015-05-06 17:29:31 -0400

    Add more options.

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 #! /usr/bin/env node
 
+var NodeGit = require("nodegit");
 var yargs = require('yargs');
 
 var argv = yargs
+    .default('hl2sdk', './hl2sdk')
     .describe('hl2sdk', 'path to the HL2SDK Git repository')
     .requiresArg('hl2sdk')
     .string('hl2sdk')
+    .default('metamod', './metamod')
     .describe('metamod', 'path to the Metamod:Source Git repository')
     .requiresArg('metamod')
     .string('metamod')
     .implies('metamod', 'hl2sdk')
+    .default('metamod-branch', '1.10-dev')
+    .describe('metamod-branch', 'branch of Metamod:Source to build')
+    .requiresArg('metamod-branch')
+    .string('metamod-branch')
+    .default('sourcemod', './sourcemod')
     .describe('sourcemod', 'path to the SourceMod Git repository')
     .requiresArg('sourcemod')
     .string('sourcemod')
     .implies('sourcemod', 'metamod')
+    .default('sourcemod-branch', '1.7-dev')
+    .describe('sourcemod-branch', 'branch of SourceMod to build')
+    .requiresArg('sourcemod-branch')
+    .string('sourcemod-branch')
+    .config('c')
+    .alias('c', 'config')
     .help('h')
     .alias('h', 'help')
     .argv;
+
+NodeGit.Repository.open(argv.hl2sdk).done();

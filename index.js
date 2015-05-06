16db4f5ec31a1bdab434d74cb784dbfcb5d98fd6
thesupremecommander <thesupremecmdr@gmail.com>
2015-05-06 16:42:18 -0400

    Add command line options.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 #! /usr/bin/env node
+
+var yargs = require('yargs');
+
+var argv = yargs
+    .describe('hl2sdk', 'path to the HL2SDK Git repository')
+    .requiresArg('hl2sdk')
+    .string('hl2sdk')
+    .describe('metamod', 'path to the Metamod:Source Git repository')
+    .requiresArg('metamod')
+    .string('metamod')
+    .implies('metamod', 'hl2sdk')
+    .describe('sourcemod', 'path to the SourceMod Git repository')
+    .requiresArg('sourcemod')
+    .string('sourcemod')
+    .implies('sourcemod', 'metamod')
+    .help('h')
+    .alias('h', 'help')
+    .argv;

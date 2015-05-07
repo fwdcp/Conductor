da07bf56a977ad47e1104d9e29fa36f900526540
thesupremecommander <thesupremecmdr@gmail.com>
2015-05-06 21:01:18 -0400

    Create robust branch checkout code.

--- index.js
+++ index.js
@@ -41,44 +41,39 @@ var argv = yargs
     .alias('h', 'help')
     .argv;
 
-var hl2sdk = path.resolve(argv.hl2sdk);
-var metamod = path.resolve(argv.metamod);
-var sourcemod = path.resolve(argv.sourcemod);
+function checkoutBranchOfRepo(path, url, branchName) {
+    return NodeGit.Repository.open(path).then(function(repo) {
+        return NodeGit.Remote.lookup(repo, 'origin').catch(function() {
+            return NodeGit.Remote.create(repo, 'origin', url);
+        }).then(function() {
+            return repo.fetchAll();
+        }).then(function() {
+            return NodeGit.Branch.lookup(repo, branchName, NodeGitBranch.BRANCH.LOCAL).then(function(branch) {
+                return repo.mergeBranches(branchName, 'origin/' + branchName);
+            }, function() {
+                return repo.getBranchCommit('origin/' + branchName).then(function(commit) {
+                    return NodeGit.Branch.create(repo, branchName, commit, true, Signature.default(repo));
+                });
+            });
+        }).then(function() {
+            repo.checkoutBranch(branchName);
+            return repo;
+        });
+    }, function() {
+        return Q.nfcall(fs.mkdirs, path)
+            .then(function() {
+                return Q.nfcall(fs.emptyDir, path);
+            })
+            .then(function() {
+                return NodeGit.Clone(branchName, path, {checkoutBranch: branchName});
+            });
+    });
+}
 
 async.auto({
-    'hl2sdk': function() {
-        return NodeGit.Repository.open(hl2sdk).catch(function() {
-            return Q.nfcall(fs.mkdirs, hl2sdk)
-                .then(function() {
-                    return Q.nfcall(fs.emptyDir, hl2sdk);
-                })
-                .then(function() {
-                    return NodeGit.Clone('https://github.com/alliedmodders/hl2sdk.git', hl2sdk, {checkoutBranch: 'tf2'});
-                });
-        });
-    },
-    'metamod': function() {
-        return NodeGit.Repository.open(metamod).catch(function() {
-            return Q.nfcall(fs.mkdirs, metamod)
-                .then(function() {
-                    return Q.nfcall(fs.emptyDir, metamod);
-                })
-                .then(function() {
-                    return NodeGit.Clone('https://github.com/alliedmodders/metamod-source.git', metamod, {checkoutBranch: argv.metamodBranch || 'master'});
-                });
-        });
-    },
-    'sourcemod': function() {
-        return NodeGit.Repository.open(sourcemod).catch(function() {
-            return Q.nfcall(fs.mkdirs, sourcemod)
-                .then(function() {
-                    return Q.nfcall(fs.emptyDir, sourcemod);
-                })
-                .then(function() {
-                    return NodeGit.Clone('https://github.com/alliedmodders/sourcemod.git', sourcemod, {checkoutBranch: argv.sourcemodBranch || 'master'});
-                });
-        });
-    },
+    'hl2sdk': checkoutBranchOfRepo(path.resolve(argv.hl2sdk), 'https://github.com/alliedmodders/hl2sdk.git', 'tf2'),
+    'metamod': checkoutBranchOfRepo(path.resolve(argv.metamod), 'https://github.com/alliedmodders/metamod-source.git', argv.metamodBranch || 'master'),
+    'sourcemod': checkoutBranchOfRepo(path.resolve(argv.sourcemod), 'https://github.com/alliedmodders/sourcemod.git', argv.sourcemodBranch || 'master'),
     'metamod-build': ['hl2sdk', 'metamod', function(results) {
         console.log(arguments);
     }]

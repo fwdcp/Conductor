8cff72b9e193e67c3f08256e1a1cece4a14c8ffb
thesupremecommander <thesupremecmdr@gmail.com>
2015-05-08 00:16:03 -0400

    Add completion indicators.

--- index.js
+++ index.js
@@ -55,28 +55,46 @@ var serverPath = path.resolve(argv._[1]);
 if (command !== 'run') {
     extend(tasks, {
         'srcds-download': function() {
-            console.log(chalk.cyan('Downloading the dedicated server for TF2...'));
-            return helpers.steamcmdUpdate(path.resolve(argv.steamcmd), 232250, 'anonymous', '');
+            console.log(chalk.cyan('Downloading the SRCDS for TF2...'));
+            return helpers.steamcmdUpdate(path.resolve(argv.steamcmd), 232250, 'anonymous', '')
+                .then(function() {
+                    console.log(chalk.cyan('SRCDS for TF2 downloaded.'));
+                });
         },
         'hl2sdk-download': function() {
             console.log(chalk.cyan('Downloading the HL2SDK for TF2...'));
-            return helpers.checkoutRepo(path.resolve(argv.hl2sdk), 'https://github.com/alliedmodders/hl2sdk.git', 'tf2');
+            return helpers.checkoutRepo(path.resolve(argv.hl2sdk), 'https://github.com/alliedmodders/hl2sdk.git', 'tf2')
+                .then(function() {
+                    console.log(chalk.cyan('HL2SDK for TF2 downloaded.'));
+                });
         },
         'metamod-download': function() {
             console.log(chalk.cyan('Downloading the Metamod:Source source...'));
-            return helpers.checkoutRepo(path.resolve(argv.metamod), 'https://github.com/alliedmodders/metamod-source.git', argv.metamodCommit);
+            return helpers.checkoutRepo(path.resolve(argv.metamod), 'https://github.com/alliedmodders/metamod-source.git', argv.metamodCommit)
+                .then(function() {
+                    console.log(chalk.cyan('Metamod:Source source downloaded.'));
+                });
         },
         'sourcemod-download': function() {
             console.log(chalk.cyan('Downloading the SourceMod source...'));
-            return helpers.checkoutRepo(path.resolve(argv.sourcemod), 'https://github.com/alliedmodders/sourcemod.git', argv.sourcemodCommit);
+            return helpers.checkoutRepo(path.resolve(argv.sourcemod), 'https://github.com/alliedmodders/sourcemod.git', argv.sourcemodCommit)
+                .then(function() {
+                    console.log(chalk.cyan('SourceMod source downloaded.'));
+                });
         },
         'metamod-build': ['hl2sdk-download', 'metamod-download', function() {
             console.log(chalk.magenta('Building Metamod:Source with AMBuild...'));
-            return helpers.ambuild(path.resolve(argv.metamod), ['--sdks=tf2'], {'HL2SDKTF2': path.resolve(argv.hl2sdk)});
+            return helpers.ambuild(path.resolve(argv.metamod), ['--sdks=tf2'], {'HL2SDKTF2': path.resolve(argv.hl2sdk)})
+                .then(function() {
+                    console.log(chalk.magenta('Metamod:Source built.'));
+                });
         }],
         'sourcemod-build': ['hl2sdk-download', 'metamod-download', 'sourcemod-download', function() {
             console.log(chalk.magenta('Building SourceMod with AMBuild...'));
-            return helpers.ambuild(path.resolve(argv.sourcemod), ['--sdks=tf2', '--no-mysql'], {'HL2SDKTF2': path.resolve(argv.hl2sdk), 'MMSOURCE_DEV': path.resolve(argv.metamod)});
+            return helpers.ambuild(path.resolve(argv.sourcemod), ['--sdks=tf2', '--no-mysql'], {'HL2SDKTF2': path.resolve(argv.hl2sdk), 'MMSOURCE_DEV': path.resolve(argv.metamod)})
+                .then(function() {
+                    console.log(chalk.magenta('SourceMod built.'));
+                });
         }]
     });
 }
@@ -85,15 +103,24 @@ if (command === 'install') {
     extend(tasks, {
         'srcds-link': ['srcds-download', function() {
             console.log(chalk.gray('Linking SRCDS files...'));
-            return helpers.mirrorLink(path.join(path.resolve(argv.steamcmd), 'steamapps', 'common', 'Team Fortress 2 Dedicated Server'), serverPath);
+            return helpers.mirrorLink(path.join(path.resolve(argv.steamcmd), 'steamapps', 'common', 'Team Fortress 2 Dedicated Server'), serverPath)
+                .then(function() {
+                    console.log(chalk.gray('SRCDS files linked.'));
+                });
         }],
         'metamod-copy': ['metamod-build', 'srcds-link', function() {
             console.log(chalk.gray('Copying Metamod:Source package...'));
-            return helpers.mirrorLink(path.join(path.resolve(argv.metamod), 'build', 'package'), serverPath);
+            return helpers.mirrorLink(path.join(path.resolve(argv.metamod), 'build', 'package'), serverPath)
+                .then(function() {
+                    console.log(chalk.gray('Metamod:Source package copied.'));
+                });
         }],
         'sourcemod-copy': ['sourcemod-build', 'metamod-copy', function() {
             console.log(chalk.gray('Copying SourceMod package...'));
-            return helpers.mirrorLink(path.join(path.resolve(argv.sourcemod), 'build', 'package'), serverPath);
+            return helpers.mirrorLink(path.join(path.resolve(argv.sourcemod), 'build', 'package'), serverPath)
+                .then(function() {
+                    console.log(chalk.gray('SourceMod package copied.'));
+                });
         }]
     });
 
